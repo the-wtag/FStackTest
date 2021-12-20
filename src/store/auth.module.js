7991d245164a08bef6d08079fe2f26506d444526
the-wtag <tanvir.hemel@welldev.io>
2021-12-20 15:53:03 +0600

    Add more auth Component

---
+++ src/store/auth.module.js
@@ -0,0 +1,61 @@
+import UserService from '../services/LoginService';
+
+const user = JSON.parse(localStorage.getItem('user'));
+const initialState = user
+    ? { status: { loggedIn: true }, user }
+    : { status: { loggedIn: false }, user: null };
+
+export const auth = {
+    namespaced: true,
+    state: initialState,
+    actions: {
+        login({ commit }, user) {
+            return UserService.login(user).then(
+                user => {
+                    commit('loginSuccess', user);
+                    return Promise.resolve(user);
+                },
+                error => {
+                    commit('loginFailure');
+                    return Promise.reject(error);
+                }
+            );
+        },
+        logout({ commit }) {
+            UserService.logout();
+            commit('logout');
+        },
+        register({ commit }, user) {
+            return UserService.register(user).then(
+                response => {
+                    commit('registerSuccess');
+                    return Promise.resolve(response.data);
+                },
+                error => {
+                    commit('registerFailure');
+                    return Promise.reject(error);
+                }
+            );
+        }
+    },
+    mutations: {
+        loginSuccess(state, user) {
+            state.status.loggedIn = true;
+            state.user = user;
+        },
+        loginFailure(state) {
+            state.status.loggedIn = false;
+            state.user = null;
+        },
+        logout(state) {
+            state.status.loggedIn = false;
+            state.user = null;
+        },
+        registerSuccess(state) {
+            state.status.loggedIn = false;
+        },
+        registerFailure(state) {
+            state.status.loggedIn = false;
+        }
+    }
+};
